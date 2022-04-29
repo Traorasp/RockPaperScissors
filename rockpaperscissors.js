2d7a6be885cf1b48609c0bfe1c8ce556992cf999
Traorasp <miguelsolanopaz@outlook.com>
2022-04-28 21:47:29 -0400

    Buttons now declare winner of the round in the console

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,40 +1,38 @@
-        function computerPlay(){
-            const playNum = Math.floor(Math.random()*3)+1;
-            if(playNum === 1) {
-                return "Rock";
-            }else if(playNum === 2) {
-                return "Paper";
-            }else {
-                return "Scissors";
-            }
-        }
+const buttons = Array.from(document.querySelectorAll('#buttonContainer'));
+buttons.forEach(button => button.addEventListener('click', playRound))
 
-        function playRound(playerSelection, computerSelection){
-            let playerSelectionLower = playerSelection.toLowerCase();
-            if(playerSelectionLower == computerSelection.toLowerCase()){
-                return "It's a tie!"
-            }else if(playerSelectionLower == "rock" && computerSelection =="Scissors"){
-                return "You win! Rock beats scissors";
-            }else if(playerSelectionLower == "paper" && computerSelection == "Rock"){
-                return "You win! Paper beats rock"
-            }else if(playerSelectionLower == "scissors" && computerSelection == "Paper"){
-                return "You win! Scissors beats paper}"
-            } else {
-                return `You lose! ${computerSelection} beats ${playerSelectionLower}`
-            }
-        }
+function computerPlay(){
+    const playNum = Math.floor(Math.random()*3)+1;
+    if(playNum === 1) {
+        return "Rock";
+    }else if(playNum === 2) {
+        return "Paper";
+    }else {
+        return "Scissors";
+    }
+}
 
-        function game(rounds) {
-            let playerScore = 0;
-            let compScore = 0;
-            for(let i = 0; i < rounds; i++){
-                let playerSelection = prompt("Rock, paper, scissors, shoot!");
-                let winner = playRound(playerSelection, computerPlay())
-                console.log(winner)
-                winner.substring(0,8) == "You win!" ? playerScore++ : winner.substring(0,9) == "You lose!" ? compScore++: compScore;
-            }
-            return playerScore > compScore ? `You win! ${playerScore} : ${compScore}` : playerScore < compScore ? `You lose! ${playerScore} : ${compScore}` : `You tie! ${playerScore} : ${compScore}`;
-        }
+function playRound(playerSelection){
+    let selection = playerSelection.target.getAttribute('class');
+    let computerSelection = computerPlay();
+    if(selection == computerSelection){ console.log("It's a tie!");}
+    else if(selection == "rock" && computerSelection =="Scissors"){console.log("You win! Rock beats scissors");}
+    else if(selection == "paper" && computerSelection == "Rock"){console.log("You win! Paper beats rock");}
+    else if(selection == "scissors" && computerSelection == "Paper"){console.log("You win! Scissors beats paper}");}
+    else {console.log(`You lose! ${computerSelection} beats ${selection}`);}
+}
 
-        console.log(game(5))
+function game(rounds) {
+    let playerScore = 0;
+    let compScore = 0;
+    for(let i = 0; i < rounds; i++){
+        let playerSelection = prompt("Rock, paper, scissors, shoot!");
+        let winner = playRound(playerSelection, computerPlay())
+        console.log(winner)
+        winner.substring(0,8) == "You win!" ? playerScore++ : winner.substring(0,9) == "You lose!" ? compScore++: compScore;
+    }
+    return playerScore > compScore ? `You win! ${playerScore} : ${compScore}` : playerScore < compScore ? `You lose! ${playerScore} : ${compScore}` : `You tie! ${playerScore} : ${compScore}`;
+}
+
+
 
